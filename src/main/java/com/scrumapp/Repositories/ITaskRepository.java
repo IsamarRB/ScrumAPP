a1980fe5b4f04eecc822a413a857abbb5c27961a
JMPolvillo <rothkildam@gmail.com>
2024-09-17 14:29:26 +0200

    updated websecurityconfig

--- src/main/java/com/scrumapp/Repositories/ITaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.scrumapp.Repositories;
-
-import com.scrumapp.model.Task;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ITaskRepository extends CrudRepository <Task, Integer> {
-}
